a61b8bc9b8e14689e6cb7a8f00d779e02601c3c1
Khalid <Khalid@Moon.wireless.concordia.ca>
2014-03-16 23:04:58 -0400

    Merge origin/master
    Conflicts:
    	src/main/java/com/soen/hasslefree/models/PhysicianAvailability.java
    	src/main/java/com/soen/hasslefree/models/User.java

---
+++ src/main/java/com/soen/hasslefree/models/PhysicianAvailability.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soen.hasslefree.models;
+
+import com.soen.hasslefree.dao.ObjectDao;
+import com.soen.hasslefree.persistence.HibernateUtil;
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.joda.time.MutableDateTime;
+
+/**
+ *
+ * @author Khalid
+ */
+@Entity
+@Table
+public class PhysicianAvailability implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long physicianAvailabilityID;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Physician relatedPhysician;
+
+    @Column
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+    private DateTime startTime;
+
+    @Column
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+    private DateTime endTime;
+
+    @OneToMany(mappedBy = "physicianAvailability", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<PhysicianTimeSlot> physicianTimeSlots = new HashSet<PhysicianTimeSlot>(0);
+
+    public long getPhysicianAvailabilityID() {
+        return physicianAvailabilityID;
+    }
+
+    public void setPhysicianAvailabilityID(long physicianAvailabilityID) {
+        this.physicianAvailabilityID = physicianAvailabilityID;
+    }
+
+    public Physician getRelatedPhysician() {
+        return relatedPhysician;
+    }
+
+    public void setRelatedPhysician(Physician relatedPhysician) {
+        this.relatedPhysician = relatedPhysician;
+    }
+
+    public DateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(DateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public DateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(DateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Set<PhysicianTimeSlot> getPhysicianTimeSlots() {
+        return physicianTimeSlots;
+    }
+
+    public void setPhysicianTimeSlots(Set<PhysicianTimeSlot> physicianTimeSlots) {
+        this.physicianTimeSlots = physicianTimeSlots;
+    }
+
+    public boolean savePhysicianAvailability() {
+        boolean hasCommted = false;
+        AppointmentType dropIn = AppointmentType.searchForAppointmentType("drop");
+        if (dropIn != null) {
+            hasCommted = generateTimeSlots(this.startTime, this.endTime, dropIn.getDuration());
+        }
+        return hasCommted;
+    }
+
+    public void updatePhysicianAvailability() throws IllegalAccessException, InvocationTargetException {
+        ObjectDao physicianAvailabilityDao = new ObjectDao();
+        physicianAvailabilityDao.updateObject(this,this.getPhysicianAvailabilityID(),PhysicianAvailability.class);
+    }
+
+    public void deletePhysicianAvailability() throws IllegalAccessException, InvocationTargetException {
+        ObjectDao physicianAvailabilityDao = new ObjectDao();
+        physicianAvailabilityDao.deleteObject(this,this.getPhysicianAvailabilityID(),PhysicianAvailability.class);
+    }
+
+    public static PhysicianAvailability getPhysicianAvailabilityById(long id) {
+        PhysicianAvailability physicianAvailabilityHolder = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            physicianAvailabilityHolder = (PhysicianAvailability) session.get(PhysicianAvailability.class, id);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return physicianAvailabilityHolder;
+    }
+
+    public static ArrayList<PhysicianAvailability> getAllPhysicianAvailabilities() {
+        ArrayList<PhysicianAvailability> physicianAvailabilities;
+        ObjectDao physicianAvailabilityDao = new ObjectDao();
+        physicianAvailabilities = physicianAvailabilityDao.getAllObjects("PhysicianAvailability");
+        return physicianAvailabilities;
+    }
+
+    public static ArrayList<PhysicianAvailability> getAllPhysicianAvailabilitiesforId(long id) {
+        ArrayList<PhysicianAvailability> physicianAvailabilities;
+        ArrayList<PhysicianAvailability> filteredAvailabilities = null;
+        ObjectDao physicianAvailabilityDao = new ObjectDao();
+        physicianAvailabilities = physicianAvailabilityDao.getAllObjects("PhysicianAvailability");
+        for (PhysicianAvailability availability : physicianAvailabilities) {
+            if (availability.getRelatedPhysician().getUserId() == id) {
+                filteredAvailabilities.add(availability);
+            }
+        }
+        return filteredAvailabilities;
+    }
+
+    public void generateTimeSlots() {
+        MutableDateTime slotStatTime = new MutableDateTime();
+        MutableDateTime slotEndTime = new MutableDateTime();
+
+        long availabilityStartTimeInMillis = availabilityStartTime.getMillis();
+        long availabilityEndTimeInMillis = availabilityEndTime.getMillis();
+
+        long availableDuration = availabilityEndTimeInMillis - availabilityStartTimeInMillis;
+        long slotDuration = dropInDurationInMinutes * 60 * 1000; // 20 min * 60 sec * 1000 millisecond
+        ArrayList<RoomTimeSlot> roomSlots = RoomTimeSlot.getFilteredAvailableRoomSlotsForDate(startTime, endTime);
+
+        if (availableDuration > 0) {
+
+            long currentSlotStartTime = availabilityStartTimeInMillis;
+            boolean stopSlicing = false;
+
+            while (!stopSlicing) {
+                //<editor-fold defaultstate="collapsed" desc="new PhysicianTimeSlot ">
+                int roomSlotIndex = hasFoundFreeRoomSlot(currentSlotStartTime, roomSlots);
+                if (roomSlotIndex < 0) {
+                    return false;
+                } else {
+                    PhysicianTimeSlot newTimeSlot = new PhysicianTimeSlot();
+                    slotStatTime.setMillis(currentSlotStartTime);
+                    slotEndTime.setMillis(currentSlotStartTime + slotDuration);
+
+                    newTimeSlot.setStartTime(slotStatTime.toDateTime());
+                    newTimeSlot.setEndTime(slotEndTime.toDateTime());
+                    newTimeSlot.setIsAvailable(true);
+                    newTimeSlot.setPhysicianAvailability(this);
+                    newTimeSlot.setRelatedPhysician(this.relatedPhysician);
+                    RoomTimeSlot roomTime = roomSlots.get(roomSlotIndex);
+                    newTimeSlot.setRelatedRoomTimeSlot(roomTime);
+
+                    roomTime.setPhysicianTimeSlot(newTimeSlot);
+                    roomTime.setIsAvailable(false);
+               
+                    //</editor-fold>
+                    this.physicianTimeSlots.add(newTimeSlot);
+                    availableDuration = availableDuration - slotDuration;
+                    currentSlotStartTime = currentSlotStartTime + slotDuration;
+                    if (availableDuration < slotDuration) { // I removed = because I want to add the last slot to the time slots.
+                        stopSlicing = true;
+                    }
+                }
+            }
+
+        }
+
+        ObjectDao physicianAvailabilityDao = new ObjectDao();
+        reserveRoomSlot(roomSlots);
+        physicianAvailabilityDao.addOrUpdateObject(this);
+        return true;
+    }
+
+    public boolean reserveRoomSlot(ArrayList<RoomTimeSlot> roomSlots) {
+        boolean hasReserved = false;
+        for (RoomTimeSlot roomSlot : roomSlots) {
+            roomSlot.updateRoomTimeSlot();
+        }
+        return hasReserved;
+    }
+
+    private int hasFoundFreeRoomSlot(long startSlotTimeInMillis, ArrayList<RoomTimeSlot> roomSlots) {
+        int index = -1;
+        for (int i = 0; i < roomSlots.size(); i++) {
+            RoomTimeSlot roomSlot = roomSlots.get(i);
+            if (roomSlot.getStartTime().getMillis() == startSlotTimeInMillis) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+}
---
+++ src/main/java/com/soen/hasslefree/models/User.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soen.hasslefree.models;
+
+import com.soen.hasslefree.dao.*;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.persistence.Entity;
+import javax.persistence.*;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+/**
+ *
+ * @author PradeepSamuel
+ */
+@ManagedBean
+@RequestScoped
+
+@Entity
+@Table(name = "user")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long userId;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String gender;
+
+    @Column
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+    private DateTime dateOfBirth;
+
+    @Column
+    private String phoneNumber;
+    
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public DateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(DateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void saveUser() {
+        ObjectDao userDao = new ObjectDao();
+        userDao.addObject(this);
+    }
+
+    public void updateUser() throws IllegalAccessException, InvocationTargetException {
+        ObjectDao userDao = new ObjectDao();
+        userDao.updateObject(this,this.userId,User.class);
+    }
+
+    public void deleteUser() throws IllegalAccessException, InvocationTargetException {
+        ObjectDao userDao = new ObjectDao();
+        userDao.deleteObject(this,this.userId,User.class);
+    }
+
+    public ArrayList<User> getAllUsers() {
+        ArrayList<User> users;
+        ObjectDao userDao = new ObjectDao();
+        users = userDao.getAllObjects("user");
+        return users;
+    }
+
+    private void clearAll() {
+        this.userId = 0;
+        this.firstName = "";
+        this.lastName = "";
+        //this.simpleDate = "";
+        this.email = "";
+        this.password = "";
+        this.gender = "";
+        this.phoneNumber = "";
+        this.dateOfBirth = null;
+        this.email = "";
+    }
+
+    public static boolean setUserRole(String emailAddress, String roleName) {
+
+        String Jdbc_Driver = "com.mysql.jdbc.Driver";
+        String Db_Url = "jdbc:mysql://localhost:3306/hasslefree";
+        String Db_UserName = "root";
+        String db_Password = "quarkuds123";
+
+        String query = "INSERT INTO user_roles(email,role_name) VALUES (?,?)";
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            Class.forName(Jdbc_Driver);
+            conn = DriverManager.getConnection(Db_Url, Db_UserName, db_Password);
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, emailAddress);
+            stmt.setString(2, roleName);
+            int result = stmt.executeUpdate();
+            return result >= 1;
+        } catch (SQLException se) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, se);
+            return false;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, se);
+                return false;
+            }
+        }
+    }
